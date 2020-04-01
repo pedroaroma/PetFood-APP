e5714327291c84e2a03d21f5406d9441b92af6b5
Pedro Luiz Aroma FIlho <pedroaroma@pop-os.localdomain>
2020-03-31 23:31:41 -0300

    projeto base

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+
+    };
+  }
+
+  componentDidMount(){
+    SplashScreen.hide();
+  }
+
+  render(){
+    
+    return(
+      <View>
+        <Text>App</Text>
+      </View>
+    );
+  }
+}
