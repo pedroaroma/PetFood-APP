429bf019cfc9aa8ca2dcb982496d1e03e393dcde
Pedro Luiz Aroma FIlho <pedroaroma@pop-os.localdomain>
2020-04-27 15:31:03 -0300

    (fix) bug ao clicar para inserir texto, o flexbox distorcia o formulario

--- src/screens/profile/Partners.js
+++ src/screens/profile/Partners.js
@@ -2,78 +2,83 @@ import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native'
 import GeneralStatusBarColor from '../../components/GeneralStatusBarColor'
 import Icon5 from 'react-native-vector-icons/FontAwesome5';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export default class Partners extends Component{
-    render() {
-    	return (
-        	<View style={styles.container}>
-        		<GeneralStatusBarColor backgrondColor="#B3C2F2" barStyle="light-content" />
-				<View style={styles.Icon}>
-					<TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-						<Icon5 name="arrow-left" size={64} color='black' />
+export default class Partners extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<ScrollView>
+					<GeneralStatusBarColor backgrondColor="#B3C2F2" barStyle="light-content" />
+					<View style={styles.Icon}>
+						<TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+							<Icon5 name="arrow-left" size={64} color='black' />
+						</TouchableOpacity>
+						<View style={styles.Name}>
+							<Text style={styles.NameFontTitle}>Petfood</Text>
+							<Text style={styles.NameFontSub}>Seja um Parceiro</Text>
+						</View>
+					</View>
+
+					<View style={styles.Form}>
+						<View style={styles.Inside}>
+							<Text style={styles.TextFormTitle}>Cadastre sua loja</Text>
+							<Text style={styles.TextFormSub}>Pensa em expandir suas negócios? Seja nosso parceiro e aumente suas vendas!</Text>
+						</View>
+						<View>
+							<Text style={styles.inputText}>Nome da Loja</Text>
+							<TextInput style={styles.input} placeholder="Qual é o nome da Loja?" placeholderTextColor="#fff"
+								secureTextEntry={false}
+							/>
+						</View>
+						<View>
+							<Text style={styles.inputText}>CNPJ</Text>
+							<TextInput style={styles.input} placeholder="qual é o CNPJ do estabelecimento?" placeholderTextColor="#fff"
+								secureTextEntry={false}
+							/>
+						</View>
+						<View>
+							<Text style={styles.inputText}>Telefone Comercial (com DDD)</Text>
+							<TextInput style={styles.input} placeholder="qual é o tel. comercial do estabelecimento?" placeholderTextColor="#fff"
+								secureTextEntry={false}
+							/>
+						</View>
+						<View>
+							<Text style={styles.inputText}>CEP</Text>
+							<TextInput style={styles.input} placeholder="qual é o CEP do estabelecimento?" placeholderTextColor="#fff"
+								secureTextEntry={false}
+							/>
+						</View>
+						<View>
+							<Text style={styles.inputText}>Endereço</Text>
+							<TextInput style={styles.input} placeholder="fornecdo pela api" placeholderTextColor="#fff"
+								secureTextEntry={false}
+							/>
+						</View>
+						<View style={styles.NumComp}>
+							<View>
+								<Text style={styles.inputText}>Número</Text>
+								<TextInput style={styles.input} placeholder="N°" placeholderTextColor="#fff"
+									secureTextEntry={false}
+								/>
+							</View>
+							<View>
+								<Text style={styles.inputText}>Complemento</Text>
+								<TextInput style={styles.input} placeholder="Ex: Casa 1, apto 22, bloco 2              " placeholderTextColor="#fff"
+									secureTextEntry={false}
+								/>
+							</View>
+						</View>
+						<View><Text></Text></View>
+					</View>
+
+					<TouchableOpacity onPress={() => { }} style={styles.buttom}>
+						<Text style={styles.buttomText}>Cadastrar Loja</Text>
 					</TouchableOpacity>
-					<View style={styles.Name}>
-						<Text style={styles.NameFontTitle}>Petfood</Text>
-						<Text style={styles.NameFontSub}>Seja um Parceiro</Text>
-					</View>
-				</View>
-				<View style={styles.Form}>
-					<View style={styles.Inside}>
-						<Text style={styles.TextFormTitle}>Cadastre sua loja</Text>
-						<Text style={styles.TextFormSub}>Pensa em expandir suas negócios? Seja nosso parceiro e aumente suas vendas!</Text>
-					</View>
-					<View>
-						<Text style={styles.inputText}>Nome da Loja</Text>
-						<TextInput style={styles.input} placeholder="Qual é o nome da Loja?" placeholderTextColor="#fff"
-						secureTextEntry={false}
-						/>
-					</View>
-					<View>
-						<Text style={styles.inputText}>CNPJ</Text>
-						<TextInput style={styles.input} placeholder="qual é o CNPJ do estabelecimento?" placeholderTextColor="#fff"
-						secureTextEntry={false}
-						/>
-					</View>
-					<View>
-						<Text style={styles.inputText}>Telefone Comercial (com DDD)</Text>
-						<TextInput style={styles.input} placeholder="qual é o tel. comercial do estabelecimento?" placeholderTextColor="#fff"
-						secureTextEntry={false}
-						/>
-					</View>
-					<View>
-						<Text style={styles.inputText}>CEP</Text>
-						<TextInput style={styles.input} placeholder="qual é o CEP do estabelecimento?" placeholderTextColor="#fff"
-						secureTextEntry={false}
-						/>
-					</View>
-					<View>
-						<Text style={styles.inputText}>Endereço</Text>
-						<TextInput style={styles.input} placeholder="fornecdo pela api" placeholderTextColor="#fff"
-						secureTextEntry={false}
-						/>
-					</View>
-					<View style={styles.NumComp}>
-					<View>
-						<Text style={styles.inputText}>Número</Text>
-						<TextInput style={styles.input} placeholder="N°" placeholderTextColor="#fff"
-						secureTextEntry={false}
-						/>
-					</View>
-					<View>
-						<Text style={styles.inputText}>Complemento</Text>
-						<TextInput style={styles.input} placeholder="Ex: Casa 1, apto 22, bloco 2              " placeholderTextColor="#fff"
-						secureTextEntry={false}
-						/>
-					</View>
-					</View>
-				</View>
-				<TouchableOpacity onPress={() => {}} style={styles.buttom}>
-					<Text style={styles.buttomText}>Cadastrar Loja</Text>
-				</TouchableOpacity>
-
-        	</View>		
-      	)
-    }
+				</ScrollView>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
@@ -89,7 +94,7 @@ const styles = StyleSheet.create({
 	Name: {
 		flex: 0.85,
 		alignItems: 'center',
-		marginTop: -7,		
+		marginTop: -7,
 	},
 	NameFontTitle: {
 		fontSize: 36,
@@ -108,7 +113,7 @@ const styles = StyleSheet.create({
 		borderRadius: 10,
 		margin: 50,
 		marginBottom: 0,
-		marginVertical: 10
+		marginVertical: 10,
 	},
 	Inside: {
 		padding: 20,
@@ -132,7 +137,7 @@ const styles = StyleSheet.create({
 		borderRadius: 5,
 		width: '90%',
 		height: 40,
-		marginLeft: 10,	
+		marginLeft: 10,
 	},
 	NumComp: {
 		flexDirection: 'row'
